772c93e96697482408183bf4b11e1e091d8fb05a
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-02-13 02:49:01 +0000

    Write documentation for malloc and free

--- src/free.c
+++ src/free.c
@@ -1,19 +1,29 @@
 #include "allocator.h"
-
-void free(void *ptr) {
-    if (ptr == NULL) {
+#include <stdio.h>
+/**
+ * free - Deallocates a previously allocated memory block
+ * @block: Pointer to the memory block to free
+ * 
+ * Marks a previously allcoated block as free for reuse.
+ * The pointer must have been returned by a previous call to malloc.
+ * If pointer is null not operation is returned.
+ */
+void free(void *block) {
+    if (block == NULL) {
         return;
     }
 
-    block_t *block_ptr = (block_t *)(ptr - 1);
+    // Return to the block header
+    block_t *block_header = (block_t *)((char *)block - sizeof(block_t));
 
     block_t *current = head_block;
 
+    // Validate that block is in out list and mark as freed.
     while (current != NULL) {
-        // What exactly is being comparied here.
-        if (current == block_ptr) {
+        if (current == block_header) {
             if (!current->free) {
                 current->free = 1;
+                printf("Block is freed\n");
             }
             return;
         }
--- src/malloc.c
+++ src/malloc.c
@@ -1,6 +1,20 @@
 #include <unistd.h>
 #include "allocator.h"
 
+/**
+ * malloc - Allocates memory blocks
+ * @size: Number of bytes to allocate
+ * 
+ * Checks for free memory blocks on the linked list. If a free block of the
+ * specified size is available, it is marked as allocated and returned.
+ * If no free block is available, uses sbrk() to create a new block and adds
+ * it to the end of the linked list.
+ * 
+ * The returned pointer points the usable memory area, after the block header.
+ * The actuall allocation includes additonal space for the block management header.
+ * 
+ * Return: Returns a pointer allocated block, or NULL new memory allocation failed.
+ */
 void* malloc(size_t size) {
     block_t *free_block = find_free_block(size);
 
@@ -12,6 +26,7 @@ void* malloc(size_t size) {
 
         block_t *new_block = sbrk(total_size);
 
+        // Check that new sbrk was able to allocate memory
         if (new_block == (void *)-1) {
             return NULL;
         }
@@ -33,6 +48,4 @@ void* malloc(size_t size) {
 
         return (void *)(new_block + 1);
     }
-    // linked list is being built backwards
-    // reuse of freed blocks isn't working
 }
