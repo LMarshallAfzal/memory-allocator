1847ee36941a873189e53982f9bc2ad7b1447de7
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-02-13 02:06:51 +0000

    Implement free function

--- src/free.c
+++ src/free.c
@@ -1,1 +1,22 @@
-// Implementation of free()
+#include "allocator.h"
+
+void free(void *ptr) {
+    if (ptr == NULL) {
+        return;
+    }
+
+    block_t *block_ptr = (block_t *)(ptr - 1);
+
+    block_t *current = head_block;
+
+    while (current != NULL) {
+        // What exactly is being comparied here.
+        if (current == block_ptr) {
+            if (!current->free) {
+                current->free = 1;
+            }
+            return;
+        }
+        current = current->next;
+    }
+}
