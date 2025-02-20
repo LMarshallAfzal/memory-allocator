9e3bca8de977c473faac81ad3d38f3bac70840c8
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-02-20 22:28:41 +0000

    Create a split_block function

--- src/allocator.c
+++ src/allocator.c
@@ -18,10 +18,41 @@ block_t *find_free_block(size_t size) {
 
     while (current != NULL) {
         if (current->free && current->size >= size) {
-            /* TODO: if current->size is >= double the size of size split the block */
+            if (current->size >= size * 2) {
+                split_block(current, size);
+            }
+
             return current;
         }
         current=current->next;
     }
     return NULL;
+}
+
+/**
+ * split_block - Splits a free memory block into two smaller blocks
+ * @block: Pointer to the block to be split
+ * @size: Size (in bytes) of the new allocated block
+ * 
+ * This function takes a large free memory block and splits it into two smaller blocks.
+ * The newly created free block is placed immediately after the allocated block.
+ * 
+ * Return: A pointer to the resized block
+ */
+block_t *split_block(block_t *block, size_t size) {
+    block_t *new_block = (block_t *)((char *)block + sizeof(block_t) + size);
+
+    new_block->size = block->size - sizeof(block_t) - size;
+    new_block->free = 1;
+    new_block->prev = block;
+    new_block->next = block->next;
+
+    block->size = size;
+    block->next = new_block;
+
+    if (new_block->next) {
+        new_block->next->prev = new_block;
+    }
+
+    return block;
 }
