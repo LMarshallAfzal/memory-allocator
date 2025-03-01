6eea476565f3fd64cf0c0a3067f256bb05ae6d9b
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-03-01 21:27:02 +0000

    Create test_coalesce_block unit test

--- tests/find_free_block/test_find_free_block.c
+++ tests/find_free_block/test_find_free_block.c
@@ -5,7 +5,6 @@
 
 void test_split_block(void) {
     void *ptr = malloc(60);
-    block_t *block = (block_t *)ptr - 1;
 
     free(ptr);
 
@@ -21,5 +20,22 @@ void test_split_block(void) {
     CU_ASSERT_EQUAL(small_block->next->size, expected_free_size);
 
     free(ptr_small);
+}
 
+void test_coalesce_block(void) {
+    void *ptr1 = malloc(5);
+    void *ptr2 = malloc(5);
+
+    free(ptr1);
+    free(ptr2);
+
+    void *ptr3 = malloc(15);
+    CU_ASSERT_PTR_NOT_NULL(ptr3);
+
+    block_t *block3 = (block_t *)ptr3 - 1;
+
+    size_t expected_block_size = 5 + sizeof(block_t) + 5;
+    CU_ASSERT_EQUAL(block3->size, expected_block_size);
+
+    free(ptr3);
 }
--- tests/find_free_block/test_find_free_block.h
+++ tests/find_free_block/test_find_free_block.h
@@ -2,5 +2,6 @@
 #define TEST_FIND_FREE_BLOCK
 
 void test_split_block(void);
+void test_coalesce_block(void);
 
 #endif
