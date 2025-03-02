f808032116097d4b2382b83827fc6215d51def96
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-03-02 22:53:07 +0000

    Write unit tests for first-fit, best-fit, and worst-fit allocation

--- tests/test_allocator.c
+++ tests/test_allocator.c
@@ -3,11 +3,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "allocator.h"
+#include "utils/test_utils.h"
 
 void test_reuse_block(void) {
-    void *block1 = malloc(5);
-    free(block1);
-    void *block2 = malloc(5);
+    TEST_SETUP();
 
-    CU_ASSERT_PTR_EQUAL(block1, block2);
+    void *block1 = malloc(100);
+    block_t *block_head1 = (block_t *)((char *)block1 - sizeof(block_t));
+
+    void *block1_address = block1;
+
+    free(block1);
+
+    void *block2 = malloc(100);
+    CU_ASSERT_EQUAL(block2, block1_address);
+
+    block_t *block_head2 = (block_t *)((char *)block2 - sizeof(block_t));
+
+    CU_ASSERT_EQUAL(block_head1, block_head2)
+    CU_ASSERT_FALSE(block_head2->free);
+
+    free(block2);
+}
+
+void test_first_fit_allocation(void) {
+    TEST_SETUP();
+    
+    set_allocation_strategy(FIRST_FIT);
+    
+    void *block1 = malloc(100);
+    void *block2 = malloc(200);
+    void *block3 = malloc(50);
+    void *block4 = malloc(300);
+    
+    free(block1);
+    free(block3);
+    
+    void *new_block = malloc(40);
+    
+    CU_ASSERT_EQUAL(new_block, block1);
+    
+    free(block2);
+    free(block4);
+    free(new_block);
+}
+
+void test_best_fit_allocation(void) {
+    TEST_SETUP();
+
+    set_allocation_strategy(BEST_FIT);
+
+    void *block1 = malloc(100);
+    void *block2 = malloc(200);
+    void *block3 = malloc(50);
+    void *block4 = malloc(300);
+    
+    free(block1);
+    free(block3);
+
+    void *new_block = malloc(40);
+
+    CU_ASSERT_EQUAL(new_block, block3);
+
+    free(block2);
+    free(block4);
+    free(new_block);
+}
+
+void test_worst_fit_allocation(void) {
+    TEST_SETUP();
+
+    set_allocation_strategy(WORST_FIT);
+    
+    void *block1 = malloc(100);
+    void *block2 = malloc(200);
+    void *block3 = malloc(50);
+    void *block4 = malloc(20);
+    void *block5 = malloc(300);
+
+    free(block1);
+    free(block3);
+    free(block5);
+
+    void *new_block = malloc(40);
+
+    CU_ASSERT_EQUAL(new_block, block5);
+
+    free(block2);
+    free(block4);
+    free(new_block);
 }
--- tests/test_allocator.h
+++ tests/test_allocator.h
@@ -2,5 +2,8 @@
 #define TEST_ALLOCATOR_H
 
 void test_reuse_block(void);
+void test_first_fit_allocation(void);
+void test_best_fit_allocation(void);
+void test_worst_fit_allocation(void);
 
 #endif
