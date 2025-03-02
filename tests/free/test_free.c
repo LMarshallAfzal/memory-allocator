51e78bdf26b39b912053dc092e45a65e816d4482
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-03-02 22:56:35 +0000

    Write unit test for freeing middle block

--- tests/free/test_free.c
+++ tests/free/test_free.c
@@ -2,9 +2,11 @@
 #include <CUnit/Basic.h>
 #include "test_free.h"
 #include "allocator.h"
-
+#include "../utils/test_utils.h"
 
 void test_free_basic(void) {
+    TEST_SETUP();
+
     void *block = malloc(10);
     block_t *block_head = (block_t *)((char *)block - sizeof(block_t));
 
@@ -17,6 +19,44 @@ void test_free_basic(void) {
 }
 
 void test_free_null(void) {
+    TEST_SETUP();
+
     free(NULL);
     CU_ASSERT(1);
+}
+
+void test_free_middle_block(void) {
+    TEST_SETUP();
+    
+    void *block1 = malloc(10);
+    void *block2 = malloc(20);
+    void *block3 = malloc(30);
+
+    block_t *block_head2 = (block_t *)((char *)block2 - sizeof(block_t));
+
+    free(block2);
+    
+    CU_ASSERT_TRUE(block_head2->free);
+    
+    block_t *block_head1 = (block_t *)((char *)block1 - sizeof(block_t));
+    block_t *block_head3 = (block_t *)((char *)block3 - sizeof(block_t));
+    
+    CU_ASSERT_EQUAL(block_head1->next, block_head2);
+    CU_ASSERT_EQUAL(block_head2->next, block_head3);
+    
+    free(block1);
+    free(block3);
+}
+
+void test_free_invalid_pointer(void) {
+    TEST_SETUP();
+    
+    char invalid_mem[16];
+    void *invalid_ptr = &invalid_mem[0];
+    
+    free(invalid_ptr);
+    
+    void *valid_block = malloc(10);
+    CU_ASSERT_PTR_NOT_NULL(valid_block);
+    free(valid_block);
 }
--- tests/test_runner.c
+++ tests/test_runner.c
@@ -7,12 +7,12 @@
 #include "test_allocator.h"
 #include "test_find_free_block.h"
 
-int setup(void) {
+int suite_setup(void) {
     head_block = NULL;
     return 0;
 }
 
-int teardown(void) {
+int suite_teardown(void) {
     block_t *current = head_block;
     block_t *next;
     while (current != NULL) {
@@ -28,23 +28,33 @@ int main() {
     CU_initialize_registry();
 
     // Test suite for malloc
-    CU_pSuite malloc_suite = CU_add_suite("malloc() Test Suite", setup, teardown);
+    CU_pSuite malloc_suite = CU_add_suite("malloc() Test Suite", suite_setup, suite_teardown);
+    CU_add_test(malloc_suite, "test_malloc_zero_size", test_malloc_zero_size);
     CU_add_test(malloc_suite, "test_malloc_basic", test_malloc_basic);
-    CU_add_test(malloc_suite, "test_malloc_multiple", test_malloc_multiple);
+    CU_add_test(malloc_suite, "test_malloc_multiple_allocations", test_malloc_multiple_allocations);
+    CU_add_test(malloc_suite, "test_malloc_large_allocation", test_malloc_large_allocation);
 
     // Test suite for free
-    CU_pSuite free_suite = CU_add_suite("free() Test Suite", setup, teardown);
+    CU_pSuite free_suite = CU_add_suite("free() Test Suite", suite_setup, suite_teardown);
     CU_add_test(free_suite, "test_free_basic", test_free_basic);
     CU_add_test(free_suite, "test_free_null", test_free_null);
+    CU_add_test(free_suite, "test_free_middle_block", test_free_middle_block);
+    CU_add_test(free_suite, "test_free_invalid_pointer", test_free_invalid_pointer);
 
     // Test suite for find free block
-    CU_pSuite find_free_block_suite = CU_add_suite("find_free_block() Test Suite", setup, teardown);
+    CU_pSuite find_free_block_suite = CU_add_suite("find_free_block() Test Suite", suite_setup, suite_teardown);
     CU_add_test(find_free_block_suite, "test_split_block", test_split_block);
+    CU_add_test(find_free_block_suite, "test_split_large_block", test_split_large_block);
+    CU_add_test(find_free_block_suite, "test_minimum_split_threshold", test_minimum_split_threshold);
+    CU_add_test(find_free_block_suite, "test_coalesce_adjacent_blocks", test_coalesce_adjacent_blocks);
+    CU_add_test(find_free_block_suite, "test_coalesce_three_blocks", test_coalesce_three_blocks);
 
     // Test suite for allocator
-    CU_pSuite allocator_suite = CU_add_suite("Allocator Test Suite", setup, teardown);
+    CU_pSuite allocator_suite = CU_add_suite("Allocator Test Suite", suite_setup, suite_teardown);
     CU_add_test(allocator_suite, "test_reuse_block", test_reuse_block);
-    CU_add_test(allocator_suite, "test_coalesce_block", test_coalesce_block);
+    CU_add_test(allocator_suite, "test_first_fit_allocation", test_first_fit_allocation);
+    CU_add_test(allocator_suite, "test_best_fit_allocation", test_best_fit_allocation);
+    CU_add_test(allocator_suite, "test_worst_fit_allocation", test_worst_fit_allocation);
 
 
     CU_console_run_tests();
